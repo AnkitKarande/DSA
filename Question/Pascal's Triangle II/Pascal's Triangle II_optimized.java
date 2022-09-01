aa6a059556c3ebf1aefbf7d6fc0d9578768305df
AnkitKarande <112334643+AnkitKarande@users.noreply.github.com>
2022-09-01 09:57:00 +0530

    Update Pascal's Triangle II_optimized.java

--- Question/Pascal's Triangle II/Pascal's Triangle II_optimized.java
+++ Question/Pascal's Triangle II/Pascal's Triangle II_optimized.java
@@ -3,6 +3,7 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         ans.add(1);
         for(int i=1;i<=rowIndex;i++){
+            //we are calculating nCr in optimized way by using the prev nCr-1 value;
             long cur = ((long)ans.get(i-1)*((rowIndex-i)+1))/i;
             ans.add((int)cur);
         }
